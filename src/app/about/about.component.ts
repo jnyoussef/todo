f5dfcdea5b8a5537d52e92fe49219ed8e687f2f3
jnyoussef <youssef.jneyah@horizon-tech.tn>
2023-01-20 15:07:56 +0100

    tp3

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { Router } from "@angular/router";
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  constructor(private route:ActivatedRoute,private router:Router) { }
-
+goals : string[] =[];
+  constructor(private route:ActivatedRoute,private router:Router, private data:DataService) { 
+    this.goals=this.data.getGoal();
+  }
   ngOnInit(): void {
   }
   sendMeHome(){
